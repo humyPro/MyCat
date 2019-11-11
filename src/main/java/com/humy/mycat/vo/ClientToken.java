f4c8fb53eff0c17cba0f5ed505bf1732b3858424
milohu <milohu@aaxischina.com>
2019-11-11 09:43:45 +0800

    Login and JWT

---
+++ src/main/java/com/humy/mycat/vo/ClientToken.java
@@ -0,0 +1,19 @@
+package com.humy.mycat.vo;
+
+import lombok.Data;
+
+/**
+ * @Author: Milo Hu
+ * @Date: 11/8/2019 13:03
+ * @Description:
+ */
+@Data
+public class ClientToken {
+
+    private String token;
+
+    private String uuid;
+
+    private Long loginTime;
+
+}
---
+++ src/main/java/com/humy/mycat/vo/Device.java
@@ -0,0 +1,19 @@
+package com.humy.mycat.vo;
+
+import lombok.Data;
+
+/**
+ * @Author: Milo Hu
+ * @Date: 11/8/2019 12:28
+ * @Description:
+ */
+@Data
+public class Device {
+
+    private String deviceId;
+
+    private String agent;
+
+    private Long loginTime;
+
+}
